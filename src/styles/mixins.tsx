476168c86ef6f43e9cc5a9b88c9569db14ff4e55
Amiko <senorbinario@tuta.io>
2022-03-31 14:49:20 -0700

    ButtonColored now accepts any props

--- src/styles/mixins.tsx
+++ src/styles/mixins.tsx
@@ -2,19 +2,18 @@ import styled from "@emotion/styled";
 import { Button } from "react-bootstrap";
 
 const BoldText = styled.p({ fontWeight: "bold" });
+
 const AllCenteredDiv = styled.div({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
 });
 
-const ButtonColored = styled(
-  ({ className, children, onClick = () => 1 }: { className?: string; children?: any; onClick?: () => any }) => (
-    <Button variant="outline-light" className={className} onClick={onClick}>
-      {children}
-    </Button>
-  )
-)(({ theme: { colors } }: any) => ({
+const ButtonColored = styled(({ className, children, ...props }: { className?: string; children?: any }) => (
+  <Button variant="outline-light" className={className} {...props}>
+    {children}
+  </Button>
+))(({ theme: { colors } }: any) => ({
   "backgroundColor": colors.buttonColor,
   "borderColor": colors.buttonColor,
   "color": colors.buttonText,
