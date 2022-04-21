0a1a0c40795ec7ba428dd96e2da07af96a90e897
Amiko <senorbinario@tuta.io>
2022-04-20 19:13:32 -0600

    The modal now has everything

--- src/types/PostProduct.type.ts
+++ src/types/PostProduct.type.ts
@@ -2,7 +2,6 @@ export default interface PostProduct {
   name: string;
   price: number;
   stock: number;
-  tags?: number[];
-  enabled?: boolean;
+  tags: number[];
   description?: string;
 }
--- src/views/Products/Products.tsx
+++ src/views/Products/Products.tsx
@@ -1,10 +1,26 @@
 import * as Yup from "yup";
 import { Fragment } from "react";
 import { useState } from "react";
+import styled from "@emotion/styled";
 import { PlusCircle } from "react-bootstrap-icons";
 import { Formik, Form as FormikForm } from "formik";
 import PostProduct from "../../types/PostProduct.type";
-import { Button, Col, Container, Row, Modal, Form, InputGroup } from "react-bootstrap";
+import { useTags } from "../../context/tagsAndCategoriesContext";
+import { Button, Col, Container, Row, Modal, Form, InputGroup, Badge } from "react-bootstrap";
+
+const Tag = styled(Badge)`
+  cursor: pointer;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  line-height: 0.7;
+  font-size: 0.8rem;
+  padding: 0.35em 0.6em;
+  border: ${(props) => (props.selected ? "2px solid blue" : "2px solid transparent")};
+`;
 
 export default function Products() {
   const [show, setShow] = useState(false);
@@ -20,10 +36,11 @@ export default function Products() {
       .required("Requerido.")
       .integer("Únicamente valores enteros")
       .moreThan(-1e-323, "Solo valores mayores o iguales a 0."),
-    tags: Yup.array(Yup.number()).optional(),
-    enabled: Yup.boolean().default(true).optional(),
-    description: Yup.string().optional(),
+    tags: Yup.array(Yup.number()),
+    description: Yup.string().optional().max(400, "Debe ser de 400 caracteres o menos."),
   });
+
+  const tags = useTags();
 
   const validate = (values: PostProduct) => {
     const errors = {};
@@ -45,19 +62,34 @@ export default function Products() {
 
       <Modal show={show} onHide={() => setShow(false)} centered size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>Nuevo producto uu</Modal.Title>
+          <Modal.Title>Nuevo producto</Modal.Title>
         </Modal.Header>
 
         <Formik
+          validate={validate}
+          validationSchema={schema}
           onSubmit={(values, { setSubmitting }) => {
             alert(JSON.stringify(values, null, 2));
             setSubmitting(false);
           }}
-          validate={validate}
-          validationSchema={schema}
-          initialValues={{ name: "", stock: 0, price: 0 }}
+          initialValues={{
+            name: "",
+            tags: [],
+            stock: undefined as unknown as number,
+            price: undefined as unknown as number,
+          }}
         >
-          {({ handleSubmit, handleBlur, handleChange, touched, values, errors, isSubmitting, isValid }) => (
+          {({
+            values,
+            errors,
+            touched,
+            isValid,
+            setValues,
+            handleBlur,
+            handleSubmit,
+            handleChange,
+            isSubmitting,
+          }) => (
             <FormikForm onSubmit={handleSubmit}>
               <Modal.Body>
                 <Row>
@@ -72,6 +104,7 @@ export default function Products() {
                         value={values.name}
                         disabled={isSubmitting}
                         onChange={handleChange}
+                        placeholder="Vestido huipil"
                         isInvalid={!!touched.name && !!errors.name}
                       />
                       <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
@@ -86,6 +119,7 @@ export default function Products() {
                       <Form.Control
                         name="price"
                         type="number"
+                        placeholder="650"
                         onBlur={handleBlur}
                         value={values.price}
                         disabled={isSubmitting}
@@ -103,6 +137,7 @@ export default function Products() {
                       <Form.Control
                         name="stock"
                         type="number"
+                        placeholder="4"
                         onBlur={handleBlur}
                         value={values.stock}
                         disabled={isSubmitting}
@@ -112,11 +147,61 @@ export default function Products() {
                       <Form.Control.Feedback type="invalid">{errors.stock}</Form.Control.Feedback>
                     </InputGroup>
                   </Form.Group>
+
+                  {/* Tags */}
+                  <Form.Group as={Col} xs={12} controlId="formStock">
+                    <Form.Label>Tags</Form.Label>
+                    <InputGroup className="mb-3">
+                      {tags?.map(({ id, name }) => (
+                        <Tag
+                          onClick={() => {
+                            const index = values.tags.indexOf(id);
+                            index === -1
+                              ? setValues((prev) => ({ ...prev, tags: [...prev.tags, id] }))
+                              : setValues((prev) => ({
+                                  ...prev,
+                                  tags: [...prev.tags.slice(0, index), ...prev.tags.slice(index + 1)],
+                                }));
+                          }}
+                          pill
+                          key={id}
+                          className="me-1 mt-1"
+                          selected={values.tags.includes(id)}
+                          bg={values.tags.includes(id) ? "primary" : "secondary"}
+                        >
+                          {name}
+                        </Tag>
+                      ))}
+                    </InputGroup>
+                  </Form.Group>
+
+                  {/* Description */}
+                  <Form.Group as={Col} xs={12} controlId="formDescription">
+                    <Form.Label>Descripción</Form.Label>
+                    <InputGroup className="mb-3" hasValidation>
+                      <Form.Control
+                        type="text"
+                        name="description"
+                        onBlur={handleBlur}
+                        placeholder="Opcional"
+                        disabled={isSubmitting}
+                        onChange={handleChange}
+                        value={values.description}
+                        isInvalid={!!touched.description && !!errors.description}
+                      />
+                      <Form.Control.Feedback type="invalid">{errors.description}</Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
                 </Row>
               </Modal.Body>
 
               <Modal.Footer>
-                <Button variant="success" type="submit" disabled={isSubmitting || !isValid}>
+                <Button
+                  className="cursor-pointer"
+                  variant="success"
+                  type="submit"
+                  disabled={isSubmitting || !isValid}
+                >
                   Crear
                 </Button>
               </Modal.Footer>
