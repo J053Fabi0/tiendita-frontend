97eb44638ac294cfc9db0e51f6d82bf415be61eb
Amiko <senorbinario@tuta.io>
2022-05-01 16:05:08 -0600

    CardPlaceholder has been updated

--- src/views/Home/Card/CardPlaceholder.tsx
+++ src/views/Home/Card/CardPlaceholder.tsx
@@ -1,8 +1,8 @@
 import { memo } from "react";
 import { BadgesDiv, Price } from "./CardComponents";
+import useBreakpoints from "../../../hooks/useBreakpoints";
 import { Placeholder, Row, Col, Card } from "react-bootstrap";
 import randomNumberInterval from "../../../utils/randomNumberInterval";
-import useBreakpoints from "../../../hooks/useBreakpoints";
 
 function CardPlaceholder() {
   const { lessOrEqualThan } = useBreakpoints();
@@ -20,8 +20,8 @@ function CardPlaceholder() {
       <div className="mt-2" />
       <BadgesDiv>
         <Placeholder animation="glow">
-          <Placeholder style={{ borderRadius: 5 }} xs={randomNumberInterval(2, 4)} bg="primary" />{" "}
-          <Placeholder style={{ borderRadius: 5 }} xs={randomNumberInterval(2, 4)} bg="primary" />
+          <Placeholder style={{ borderRadius: 5 }} xs={randomNumberInterval(2, 4)} bg="secondary" />{" "}
+          <Placeholder style={{ borderRadius: 5 }} xs={randomNumberInterval(2, 4)} bg="secondary" />
         </Placeholder>
       </BadgesDiv>
       <div className="mt-2" />
@@ -29,14 +29,6 @@ function CardPlaceholder() {
       <Placeholder className="d-none d-md-block d-lg-none mb-1 mt-1" as="p" animation="glow">
         <Placeholder xs={4} />
       </Placeholder>
-
-      {lessOrEqualThan.small ? null : (
-        <Row>
-          <Col xs={12} sm={12} md={12} lg={7}>
-            <Placeholder.Button variant="primary" xs={12} />
-          </Col>
-        </Row>
-      )}
     </>
   );
 }
