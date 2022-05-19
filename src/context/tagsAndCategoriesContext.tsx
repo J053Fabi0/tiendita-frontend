2f3783f76b010bfcf74447642439e6aa5416b5a7
Amiko <senorbinario@tuta.io>
2022-05-18 22:17:01 -0600

    tagsAndCategories now uses the LoadData hook

--- src/context/tagsAndCategoriesContext.tsx
+++ src/context/tagsAndCategoriesContext.tsx
@@ -4,6 +4,7 @@ import Tag from "../types/tags.type";
 import Category from "../types/category.type";
 import { useAuthTokenReady } from "./personContext";
 import { useContext, createContext, useState, useEffect } from "react";
+import useLoadData from "../hooks/useLoadData";
 
 const TagsContext = createContext<Tag[] | null>(null);
 const TagsAndCategoriesContext = createContext<Category[] | null>(null);
@@ -16,26 +17,13 @@ export function TagsAndCategoriesProvider(a: { children: any }) {
   const [tags, setTags] = useState<null | Tag[]>(null);
   const [tagsAndCategories, setTagsAndCategories] = useState<null | Category[]>(null);
 
-  useEffect(() => {
-    (async () => {
-      if (!authTokenReady) {
-        setTags(null);
-        setTagsAndCategories(null);
-        return;
-      }
+  useLoadData<null | Category[]>(
+    [authTokenReady],
+    setTagsAndCategories,
+    () => http.get<{ message: Category[] }>("/tags"),
+    { defaultValue: null }
+  );
 
-      let message: null | Category[] = null;
-      while (message === null)
-        try {
-          message = (await http.get<{ message: Category[] }>("/tags")).data.message;
-        } catch (e) {
-          console.error(e);
-          await sleep(1000);
-        }
-
-      setTagsAndCategories(message);
-    })();
-  }, [authTokenReady]);
   useEffect(() => {
     if (tagsAndCategories === null) return;
     setTags(tagsAndCategories.map((a) => a.tags).flat());
