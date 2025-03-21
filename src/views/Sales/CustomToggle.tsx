5eb86f4984c360c08b31ffb0d1cea818204d4b03
Jose Fabio <J053Fabi0@users.noreply.github.com>
2025-03-21 14:27:51 -0600

    Change color of CustomToggle button

--- src/views/Sales/CustomToggle.tsx
+++ src/views/Sales/CustomToggle.tsx
@@ -1,9 +1,19 @@
+import styled from "@emotion/styled";
 import { Nav, useAccordionButton } from "react-bootstrap";
 
-const CustomToggle = ({ children, eventKey }: { children: any; eventKey: string }) => (
-  <Nav.Item onClick={useAccordionButton(eventKey)}>
+const CustomToggle = ({ children, eventKey, ...props }: { children: unknown; eventKey: string }) => (
+  <Nav.Item {...props} onClick={useAccordionButton(eventKey)}>
     <Nav.Link eventKey={eventKey}>{children}</Nav.Link>
   </Nav.Item>
 );
 
-export default CustomToggle;
+export default styled(CustomToggle)`
+  & > a.active {
+    background-color: ${(props) => props.theme.colors.buttonColor} !important;
+    color: white !important;
+  }
+
+  & > a {
+    color: ${(props) => props.theme.colors.buttonColor} !important;
+  }
+`;
